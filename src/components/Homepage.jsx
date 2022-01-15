9b073c154f2c4027aaef57d80f815318297646ba
Aditya <adityapradhan11@gmail.com>
2022-01-15 22:17:12 +0530

    Layout Compeleted

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import millify from 'millify'; // will use to format numbers 
+
+const Homepage = () => {
+    return (
+        <div>
+            Homepage
+        </div>
+    )
+}
+
+export default Homepage
